2225e4fd3aedd7c598f2ddcf283cbcfffcc55139
Ramona Lind <ramona.lind@smail.fh-koeln.de>
2014-05-07 13:18:47 +0200

    GspApp

---
+++ TestApp/src/com/example/testapp/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.testapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		if (savedInstanceState == null) {
+			getSupportFragmentManager().beginTransaction()
+					.add(R.id.container, new PlaceholderFragment()).commit();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	/**
+	 * A placeholder fragment containing a simple view.
+	 */
+	public static class PlaceholderFragment extends Fragment {
+
+		public PlaceholderFragment() {
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View rootView = inflater.inflate(R.layout.fragment_main, container,
+					false);
+			return rootView;
+		}
+	}
+	LocationProvider provider = lm.getProvider("gps");
+	lm.requestLocationUpdates("gps",
+	        60000, // 1min
+	        1,   // 10m
+	        locationListener);
+	private final LocationListener locationListener = new LocationListener() {
+	      public void onLocationChanged(Location l) {
+	         TextView tv = new TextView(gpstracker.this);
+	         tv.setText("lat: " + l.getLatitude() + "\nlon: " + l.getLongitude());
+	         setContentView(tv);
+	      }
+}
